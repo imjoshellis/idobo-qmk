40a8f3c55ba55a62ffbb187fa004e773d88b28bb
Josh Ellis <joshe523@gmail.com>
2020-07-06 15:31:04 -0500

    Add layer toggle on F

--- keymap.c
+++ keymap.c
@@ -78,11 +78,14 @@ void    lthumb_reset(qk_tap_dance_state_t *state, void *user_data);
 
 // ***** Definitions *****
 
+// Layers  
+#define F_TGL   LT(3, KC_F)      // F    || L3
+#define LT_SPC  TD(TD_LTHUMB)    // SPC  || TD LTHUMB 
+#define RT_SPC  LT(1, KC_SPC)    // SPC  || L1
+
 // Tap / Hold
 #define LC_ESC  LCTL_T(KC_ESC)   // TAB  || CTRL 
-#define LCA_ESC LCA_T(KC_ESC)    // ESC  || L2 
-#define LT_SPC  TD(TD_LTHUMB)    // SPC  || TD LTHUMB 
-#define RT_SPC  LT(1, KC_SPC)    // SPC  || TD LTHUMB 
+#define LCA_ESC LCA_T(KC_ESC)    // ESC  || LCA 
 #define SFT_TAB LSFT_T(KC_TAB)   // TAB  || SFT 
 #define SG_BSPC  SGUI_T(KC_BSPC)   // SGUI || ENT 
 
@@ -132,7 +135,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    Z_OUT,   Z_RESET, Z_IN,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, 
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    FLLSCRN, OSL(3),  DIS_NCH, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, 
-        LC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MUTE, KC_VOLD, KC_VOLU, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, 
+        LC_ESC,  KC_A,    KC_S,    KC_D,    F_TGL,   KC_G,    KC_MUTE, KC_VOLD, KC_VOLU, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, 
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MPRV, KC_MPLY, KC_MNXT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, 
         MO(3),   LCA_ESC, KC_LCTL, KC_LALT, KC_LGUI, LT_SPC,  SFT_TAB, _______, SG_BSPC,  RT_SPC,  CMD_TAB, OSL(3),  N_PD,    N_ND,    MO(12)
         ),
